2eadfbebecc459e94163caf10f89084a79558b30
Aayush Gajera <aayushgajera2000@gmail.com>
2024-05-23 13:13:50 +0530

    remove unneccessary file from project

--- redux_exp2/src/state/reducers/index.js
+++ redux_exp2/src/state/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import usersReducer from "./usersReducer";
 import currentCountReducer from "./currentCountReducer";
 import tableReducer from './tableReducer';
 
 const reducers = combineReducers({
-    users: usersReducer,
     currentCount: currentCountReducer,
     table: tableReducer
 
