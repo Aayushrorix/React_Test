38cfff68f02c5e53d716d8634613295a3040a0ab
Aayush Gajera <aayushgajera2000@gmail.com>
2024-05-23 15:47:46 +0530

    changes in Navbar

--- redux_exp2/src/components/header.js
+++ redux_exp2/src/components/header.js
@@ -4,8 +4,8 @@ const Header = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link to='/' className="navbar-brand">Home</Link>
-                <Link to='/add' className="navbar-brand">Add New User</Link>
+                <Link style={{backgroundColor:"#7abd28",color:"white",padding:"5px"}} to='/' className="navbar-brand">Home</Link>
+                <Link style={{backgroundColor:"#3464eb",color:"white",padding:"5px"}} to='/add' className="navbar-brand">Add New User</Link>
             </nav>
         </div>
     )
