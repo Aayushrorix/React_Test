5d7cf5f519322357ff6e168df9b6f72133c7ec9a
Aayush Gajera <aayushgajera2000@gmail.com>
2024-05-27 17:09:09 +0530

    On Refresh data will remain as it is

--- redux_exp2/src/state/store.js
+++ redux_exp2/src/state/store.js
@@ -1,6 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducers from './reducers/index';
 
-const store = configureStore({reducer:reducers,middleware: (getDefaultMiddleware) => getDefaultMiddleware()}) 
+const saveToLocalStorage = (state) => {
+    try {
+        localStorage.setItem('state', JSON.stringify(state));
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+const loadFromLocalStorage = () => {
+    try {
+        const stateStr = localStorage.getItem('state');
+        return stateStr ? JSON.parse(stateStr) : undefined;
+    } catch (e) {
+        console.error(e);
+        return undefined;
+    }
+};
+
+const preloadedState = loadFromLocalStorage();
+
+const store = configureStore({reducer:reducers,preloadedState,middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false,})})
+
+store.subscribe(() => {
+    saveToLocalStorage(store.getState());
+});
 
 export default store;
