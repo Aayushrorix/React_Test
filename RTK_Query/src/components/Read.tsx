91e6f3a093fa11a608dadcc386a816606845baa2
Aayush Gajera <aayushgajera2000@gmail.com>
2024-06-11 17:07:59 +0530

    Change view design apply grid view

--- RTK_Query/src/components/Read.tsx
+++ RTK_Query/src/components/Read.tsx
@@ -15,25 +15,24 @@ function Read() {
   return (
     <div className='container mx-auto'>
       <h2>Read Operation</h2>
-      <div className='row'>
+      <div className='container-grid row'>
         {isLoading && <span>Loading...</span>}
         {isError && <span>Something went wrong </span>}
         {isSuccess &&
-          data?.map((student)=>(
-          <div key={student?.id} className='col-3'>
-          <div className="card" style={{width:'18rem',margin:'4px'}}>
-            <div className="card-body">
-              <h5 className="card-title">{student?.studentName}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">{student?.studentEmail}</h6>
-              {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
-              <button className="card-link" onClick={() => deleteStudent(student?.id)}>Delete</button>
-              <button className="card-link"><Link to={`/edit/${student?.id}`}>Edit</Link></button>
+          data?.map((student) => (
+          <div key={student?.id} className='grid-item col-md-4 col-sm-6 mb-4'>
+            <div className='card h-100' style={{border: '1px solid'}}>
+              <div className='card-body'>
+                <h5 className='card-title'>{student?.studentName}</h5>
+                <h6 className='card-subtitle mb-2 text-muted'>{student?.studentEmail}</h6>
+                <button className='btn btn-link p-0' onClick={() => deleteStudent(student?.id)}>Delete</button>
+                <Link to={`/edit/${student?.id}`} className='btn btn-link p-0'>Edit</Link>
+              </div>
             </div>
-          </div>
           </div>
         ))}
       </div>
-    </div>
+  </div>
   )
 }
 
