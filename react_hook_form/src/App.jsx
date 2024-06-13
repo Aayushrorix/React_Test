a0352735c967e2a690ba022e2a9be0a9f30ccb1b
Aayush Gajera <aayushgajera2000@gmail.com>
2024-06-13 18:59:25 +0530

    Add error message to form

--- react_hook_form/src/App.jsx
+++ react_hook_form/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import { useForm } from 'react-hook-form'
 
 function App() {
-  const {register, handleSubmit, reset  } = useForm();
+  const {register, handleSubmit, reset, formState: { errors }} = useForm();
 
   const onSubmit = (data) => {
     console.log(data)
@@ -21,74 +21,45 @@ function App() {
             <div className="field">
               <label htmlFor="fullname">Full Name</label>
               <input type="text" className="form-control" id="fullname" name="fullname" {...register('fullname', { required: true })}/>
+              {errors.fullname && <span className="error">Full Name is required</span>}
             </div>
 
             <div className="field">
               <label htmlFor="email">Email address</label>
               <input type="email" className="form-control" id="email" name="email" {...register('email', { required: true })}/>
+              {errors.email && <span className="error">Email is required</span>}
             </div>
 
             <div className="field">
               <label htmlFor="phone">Phone Number</label>
               <input type="text" className="form-control" id="phone" name="phone" {...register('phone', { required: true })}/>
+              {errors.phone && <span className="error">Phone Number is required</span>}
             </div>
 
-            {/* <div className="field">
-              <label htmlFor="password">Password</label>
-              <input type="password" className="form-control" id="password" name="password" {...register('password', { required: true })}/>
+            {/* Repeat similar error handling for other fields */}
+            
+            <div className="field">
+              <label htmlFor="addressLine1">Address Line 1</label>
+              <input type="text" className="form-control" id="addressLine1" name="addressLine1" {...register('addressLine1', { required: true })}/>
+              {errors.addressLine1 && <span className="error">Address Line 1 is required</span>}
             </div>
-
-            <div className='field'>
-              <div className="form-check form-check-inline">
-                <input className="form-check-input"name='gender' {...register('gender', { required: true })} type="radio" id="male" value="male" />
-                <label className="form-check-label" htmlFor="male">male</label>
-              </div>
-              <div className="form-check form-check-inline">
-                <input className="form-check-input"name='gender' {...register('gender', { required: true })} type="radio" id="female" value="female" />
-                <label className="form-check-label" htmlFor="female">female</label>
-              </div>
-              <div className="form-check form-check-inline">
-                <input className="form-check-input"name='gender' {...register('gender', { required: true })} type="radio" id="other" value="other" />
-                <label className="form-check-label" htmlFor="other">other</label>
-              </div>
-            </div>
-
-            <div className='field'>
-              <select className="custom-select" name='state' {...register('state', { required: true })}>
-                <option value="" defaultChecked>Select your state</option>
-                <option value="Delhi">Delhi</option>
-                <option value="Punjab">Punjab</option>
-                <option value="Jharkhand">Jharkhand</option>
-                <option value="Bihar">Bihar</option>
-              </select>
-            </div>
-
-            <div className='field'>
-              <div className="form-check form-check-inline">
-                <input className="form-check-input" type="checkbox" {...register('terms_and_conditions', { })} name='terms_and_conditions' id="terms" value="agree" />
-                <label className="form-check-label" htmlFor="terms">I agree all terms and conditons</label>
-              </div>
-              <div className="form-check form-check-inline">
-                <input className="form-check-input" type="checkbox" {...register('send_me_updates', {  })} name='send_me_updates' id="updates" />
-                <label className="form-check-label" htmlFor="updates">send me latest Bootstrap updates</label>
-              </div>
-            </div> */}
 
             <div className="field">
-              <label htmlFor="line1">Address Line 1</label>
-              <input type="text" className="form-control" id="line1" name="address[0]" {...register('address[0]', { required: true })}/>
+              <label htmlFor="addressLine2">Address Line 2</label>
+              <input type="text" className="form-control" id="addressLine2" name="addressLine2" {...register('addressLine2', { required: true })}/>
+              {errors.addressLine2 && <span className="error">Address Line 2 is required</span>}
             </div>
-            <div className="field">
-              <label htmlFor="line2">Address Line 2</label>
-              <input type="text" className="form-control" id="line2" name="address[1]" {...register('address[1]', { required: true })}/>
-            </div>
+
             <div className="field">
               <label htmlFor="city">City</label>
-              <input type="text" className="form-control" id="city" name="address.city" {...register('address.city', { required: true })}/>
+              <input type="text" className="form-control" id="city" name="city" {...register('city', { required: true })}/>
+              {errors.city && <span className="error">City is required</span>}
             </div>
+
             <div className="field">
               <label htmlFor="zip">Zip</label>
-              <input type="text" className="form-control" id="zip" name="address.zip" {...register('address.zip', { required: true })}/>
+              <input type="text" className="form-control" id="zip" name="zip" {...register('zip', { required: true })}/>
+              {errors.zip && <span className="error">Zip is required</span>}
             </div>
 
             <button type='submit' className='btn-add'>Submit</button>
