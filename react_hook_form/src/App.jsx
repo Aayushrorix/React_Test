62979875509fbca2d63296c873beb4341eab2a77
Aayush Gajera <aayushgajera2000@gmail.com>
2024-06-13 16:26:23 +0530

    Change design of form

--- react_hook_form/src/App.jsx
+++ react_hook_form/src/App.jsx
@@ -13,32 +13,32 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div className="container py-5">
-        <div className="card border-0 shadow p-4 w-50 mx-auto">
+    <div>
+      <div >
+        <div className='div-form'>
           <form onSubmit={handleSubmit(onSubmit)}>
 
-            <div className="form-group">
+            <div className="field">
               <label htmlFor="fullname">Full Name</label>
               <input type="text" className="form-control" id="fullname" name="fullname" {...register('fullname', { required: true })}/>
             </div>
 
-            <div className="form-group">
+            <div className="field">
               <label htmlFor="email">Email address</label>
               <input type="email" className="form-control" id="email" name="email" {...register('email', { required: true })}/>
             </div>
 
-            <div className="form-group">
+            <div className="field">
               <label htmlFor="phone">Phone Number</label>
               <input type="text" className="form-control" id="phone" name="phone" {...register('phone', { required: true })}/>
             </div>
 
-            {/* <div className="form-group">
+            {/* <div className="field">
               <label htmlFor="password">Password</label>
               <input type="password" className="form-control" id="password" name="password" {...register('password', { required: true })}/>
             </div>
 
-            <div className='form-group'>
+            <div className='field'>
               <div className="form-check form-check-inline">
                 <input className="form-check-input"name='gender' {...register('gender', { required: true })} type="radio" id="male" value="male" />
                 <label className="form-check-label" htmlFor="male">male</label>
@@ -53,7 +53,7 @@ function App() {
               </div>
             </div>
 
-            <div className='form-group'>
+            <div className='field'>
               <select className="custom-select" name='state' {...register('state', { required: true })}>
                 <option value="" defaultChecked>Select your state</option>
                 <option value="Delhi">Delhi</option>
@@ -63,7 +63,7 @@ function App() {
               </select>
             </div>
 
-            <div className='form-group'>
+            <div className='field'>
               <div className="form-check form-check-inline">
                 <input className="form-check-input" type="checkbox" {...register('terms_and_conditions', { })} name='terms_and_conditions' id="terms" value="agree" />
                 <label className="form-check-label" htmlFor="terms">I agree all terms and conditons</label>
@@ -74,24 +74,24 @@ function App() {
               </div>
             </div> */}
 
-            <div className="form-group">
+            <div className="field">
               <label htmlFor="line1">Address Line 1</label>
               <input type="text" className="form-control" id="line1" name="address[0]" {...register('address[0]', { required: true })}/>
             </div>
-            <div className="form-group">
+            <div className="field">
               <label htmlFor="line2">Address Line 2</label>
               <input type="text" className="form-control" id="line2" name="address[1]" {...register('address[1]', { required: true })}/>
             </div>
-            <div className="form-group">
+            <div className="field">
               <label htmlFor="city">City</label>
               <input type="text" className="form-control" id="city" name="address.city" {...register('address.city', { required: true })}/>
             </div>
-            <div className="form-group">
+            <div className="field">
               <label htmlFor="zip">Zip</label>
               <input type="text" className="form-control" id="zip" name="address.zip" {...register('address.zip', { required: true })}/>
             </div>
 
-            <button type='submit' className='btn btn-primary'>Submit</button>
+            <button type='submit' className='btn-add'>Submit</button>
           </form>
         </div>
       </div>
